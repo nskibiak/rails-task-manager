d53286d1ad9defab38b7dfbfdbd6cf4f4397ef09
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-01-08 11:57:55 +0900

    added tasks to db

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,17 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  def new
+  end
+  def create
+  end
+  def show
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end
 end
