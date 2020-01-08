7ca5400443020fb979993ebb27de1402c30db3f7
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-01-08 16:27:08 +0900

    added create action and view, as well as bootstrap styling on new form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,14 +7,25 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
-  # def new
-  # end
-  # def create
-  # end
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
   # def edit
   # end
   # def update
   # end
   # def destroy
   # end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,20 +4,14 @@ Rails.application.routes.draw do
   # resources :tasks
 
   get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: 'new'
+
+  post 'tasks', to: 'tasks#create'
+
   get 'tasks/:id', to: 'tasks#show', as: 'task'
-
-
-  # get    "tasks",          to: "tasks#index"
-
-  # get    "tasks/new",      to: "tasks#new"
-  # post   "tasks",          to: "tasks#create"
-
-  # # NB: The `show` route needs to be *after* `new` route.
-  # get    "tasks/:id",      to: "tasks#show"
 
   # get    "tasks/:id/edit", to: "tasks#edit"
   # patch  "tasks/:id",      to: "tasks#update"
 
   # delete "tasks/:id",      to: "tasks#destroy"
-
 end
