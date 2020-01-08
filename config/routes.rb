fb9bfffac05053c5bd3881c9b8583bbaeb4ffd4b
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-01-08 15:57:05 +0900

    replaced resources with correct routing syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :tasks
+  # resources :tasks
 
-  # get 'tasks', to: 'tasks#index'
-  # get 'tasks/:id', to: 'tasks#show'
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 
 
   # get    "tasks",          to: "tasks#index"
