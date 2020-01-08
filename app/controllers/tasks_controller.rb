307e404076ab64179b222fd4137d8cbf92aa97b6
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-01-08 12:02:38 +0900

    renamed erb file with correct extension

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,16 +2,16 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
-  def new
-  end
-  def create
-  end
-  def show
-  end
-  def edit
-  end
-  def update
-  end
-  def destroy
-  end
+  # def new
+  # end
+  # def create
+  # end
+  # def show
+  # end
+  # def edit
+  # end
+  # def update
+  # end
+  # def destroy
+  # end
 end
